fafdf8494f2cd9d61202d9f233cce62304bc04b4
AntogamerYT <antoniocavaliere2009@gmail.com>
2022-09-03 12:31:14 +0200

    still doesnt work

--- main.py
+++ main.py
@@ -13,25 +13,54 @@ TOKEN = os.getenv("TOKEN")
 
 client = commands.Bot(command_prefix=':', self_bot=True, help_command=None)
 
+
 print(Fore.RED+"I, ANTOGAMER, AM NOT RESPONSIBLE FOR ANY OF YOUR ACCOUNT BANS MADE FROM THIS USERBOT, PROCEED AT YOUR RISK, YOU CAN QUIT BY USING CTRL+C!!!")
 time.sleep(0.5)
 print(Fore.WHITE+"Proceeding in 5 seconds...")
 time.sleep(5)
 text = input("Input the text that you would like to send to the channel (this will cover all the inputs asked from the bot):")
+
+if text == "":
+ text = input("Please input some text to continue:")
+
 print(" ")
 image = input("Please input the image that you would like to send on the report, just press enter to skip:")
 
+def checkifimage(link):
+	try:
+	 image = Image.open(requests.get(link, stream=True).raw)
+	except:
+	 pass
+	 return False
+	return True
 
-
+print(checkifimage(image))
 @client.event
 async def on_ready():
     print(f'Logged in as {client.user} (ID: {client.user.id})')
-    me = await client.fetch_user("658586788721590273")
-    await me.send("it works! debug: " + text)
+    bugbot = await client.fetch_user("658586788721590273")
     #bugbot = await client.fetch_user("874342935909531678")
-    #while True:
-#	await bugbot.send("/report")
-#	await asyncio.sleep(0.2)
+    counter = 0
+    while True:
+     await bugbot.send("/report")
+     await asyncio.sleep(0.2)
+     await bugbot.send("YES")
+     await asyncio.sleep(0.3)
+     await bugbot.send(text)
+     await asyncio.sleep(0.3)
+     await bugbot.send(text)
+     await asyncio.sleep(0.3)
+     await bugbot.send(text)
+     await asyncio.sleep(0.3)
+     await bugbot.send(text)
+     await asyncio.sleep(0.3)
+     if image != "":
+       await bugbot.send(image)
+     else:
+       await bugbot.send("no")
+     counter = counter+1
+     print("Completed {counter} bug report(s), going to the next one in 3 seconds!")
+     await asyncio.sleep(3)
 	
 
 client.run(TOKEN)
